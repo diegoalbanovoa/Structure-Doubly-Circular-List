8ee6fd55fc92ddfbd84f7478ce8f8f8f0e22cdd4
Diego Fernando Alba Novoa <85304078+diegoalbanovoa@users.noreply.github.com>
2021-10-19 15:17:35 -0500

    Casos de Prueba

--- src/TestDoubleList/TestDoublyLinkedList.java
+++ src/TestDoubleList/TestDoublyLinkedList.java
@@ -11,52 +11,68 @@ import DoubleList.DoublyLinkedList;
  */
 public class TestDoublyLinkedList {
 	public static void main(String[] args) {
-		
-		   //Creates CDoublyLinkedList cdll - 1, 2, 3, 4
-		
-		   DoublyLinkedList<Integer> cdll = new DoublyLinkedList<Integer>(); 
-		   
-		   cdll.addElement(1);        //adds items to the list
-		   cdll.addElement(2);
-		   cdll.addElement(3);
-		   cdll.addElement(4);
-		   
-		   System.out.println("\nOriginal list: ");
-		   System.out.println(cdll.ShowList());  //prints the original list
-		   											//Expected output 1,2,3,4
-		   
-		   System.out.println("\naddFront(9)");
-		   cdll.addFront(9);                      //adds new item at the front of the list 
-		   System.out.println(cdll.ShowList());   //Expected output 9,1,2,3,4
-		   
-		   System.out.println("\naddBack(8)");
-		   cdll.addBack(8);							//adds new item at the back of the list
-		   System.out.println(cdll.ShowList());		//Expected output 9,1,2,3,4,8
-		   
-		   System.out.println("\naddAtPosition(6,2)");
-		   cdll.addAtPosition(6, 2);					//inserts new item at the specified position
-		   System.out.println(cdll.ShowList());		//Expected output 9,1,6,2,3,4,8
-		   
-		   System.out.println("\nremove(4)");
-		   cdll.remove(4);							//removes the specified item from the list
-		   System.out.println(cdll.ShowList());		//Expected output 9,1,6,2,3,8
-		   
-		   System.out.println("\nremoveFront()");
-		   cdll.removeFront();						//removes the front item from the list
-		   System.out.println(cdll.ShowList());		//Expected output 1,6,2,3,8
-		   
-		   System.out.println("\nremoveBack()");
-		   cdll.removeBack();						//removes the last item  from the list
-		   System.out.println(cdll.ShowList());		//Expected output 1,6,2,3
-		   
-		   System.out.println("\nremoveAtPosition(1)");
-		   cdll.removeAtPosition(1);					//removes the item in the specified position
-		   System.out.println(cdll.ShowList());		//Expected output 1,2,3
-		   
-		   System.out.println("\nprintReverse()");
-		   System.out.println(cdll.showPrintReverse());   //prints the list backwards
-		   										      //Expected output 3,2,1
-		   
+		System.out.println(
+				"----------------------------------------> Casos de prueba <-----------------------------------------\n");
+
+		DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<Integer>();
+
+		doublyLinkedList.addElement(1); // adds items to the list
+		doublyLinkedList.addElement(2);
+		doublyLinkedList.addElement(3);
+		doublyLinkedList.addElement(4);
+		System.out.println(
+				"----------------------------------------> Lista Original <------------------------------------------\n");
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"-----------------------> Agregar el elemento 9 al frente de la lista <------------------------------");
+		doublyLinkedList.addFront(9);
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"---------------------> Agregar el elemento 8 al final de la lista <---------------------------------");
+		doublyLinkedList.addBack(8);
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+
+		System.out.println(
+				"-------------------> Agregar el elemento en la posicion 2 elemento 6 <------------------------------");
+		doublyLinkedList.addAtPosition(6, 2);
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"----------------------------> Remover el elemento 4 de la lista <-----------------------------------");
+		doublyLinkedList.remove(4);
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"-------------------------> Remover el primer elemento de la lista <---------------------------------");
+		doublyLinkedList.removeFront();
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"------------------------> Remover el ultimo elemento de la lista <----------------------------------");
+		doublyLinkedList.removeBack();
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println(
+				"------------------------> Remover el elemento en la posicion 1 de la lista <------------------------");
+		doublyLinkedList.removeAtPosition(1);
+		System.out.println(doublyLinkedList.ShowList());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
+		System.out.println("-------------------------------> Mostrar lista en orden inverso <-----------------------------------");
+
+		System.out.println(doublyLinkedList.showPrintReverse());
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------\n");
 	}
 
 }
